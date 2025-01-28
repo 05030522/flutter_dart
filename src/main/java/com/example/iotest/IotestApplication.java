8e135aaf53fd41f2e4d0b1fe65921b5382849803
05030522 <kjinhx96@gmail.com>
2025-01-28 22:44:02 +0900

    iotest

---
+++ src/main/java/com/example/iotest/IotestApplication.java
@@ -0,0 +1,13 @@
+package com.example.iotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iotest/IotestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
